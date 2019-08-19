c89f498d97936c16fb2a31178aad4a08ca57ac4b
Ranika Williams <rnkprice07@gmail.com>
2019-08-19 16:47:17 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -2,8 +2,6 @@ class CreateGenres < ActiveRecord::Migration[4.2]
   def change
     create_table :genres do |t|
       t.string :name
-      t.integer :artist_id
-      t.integer :genre_id
     end
   end
 end
